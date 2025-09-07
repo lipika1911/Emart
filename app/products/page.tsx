1e7043209a34db7facab0d5f721a503e6a699bc4
lipika1911 <lipikaarya1723@gmail.com>
2025-09-07 21:08:12 +0530

    feat: create products page with search functionality

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,16 @@
-export default function ProductsPage(){
+import { ProductList } from "@/components/product-list"
+import { stripe } from "@/lib/stripe"
+
+export default async function ProductsPage(){
+    const products = await stripe.products.list({
+        expand: ["data.default_price"],
+    })
     return(
-        <div>
-            Products Page
+        <div className="pb-8">
+            <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+                All Products
+            </h1>
+            <ProductList products={products.data} />
         </div>
     )
 }
